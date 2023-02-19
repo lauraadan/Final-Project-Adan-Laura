735ecf43e348fb87c875395e11c32fa747dc7ef9
lauraadan <info@lauraadan.com>
2023-02-19 13:32:13 +0100

    init

---
+++ docs/assets/file__plugin-vue_export-helper.99d5da26.js
@@ -0,0 +1,1 @@
+const p="/Todo-App-Vue3/assets/file_logo-todo.9277f302.png",r=(o,s)=>{const t=o.__vccOpts||o;for(const[e,_]of s)t[e]=_;return t};export{r as _,p as a};
---
+++ docs/assets/file_auth.dad203f1.js
@@ -0,0 +1,1 @@
+import{i}from"./file_index.2f5480c0.js";const u=i("user",{state:()=>({isAuth:!1,user:{id:"",email:"",pasword:""}}),actions:{login(s,t,e){this.isAuth=!this.isAuth,this.user.id=s,this.user.email=t,this.user.password=e},logout(){this.isAuth=!this.isAuth,this.user.email="",this.user.password=""}},persist:{enabled:!0,strategies:[{key:"user",storage:localStorage}]}});export{u};
